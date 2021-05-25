767362c710f468a9835d15281aa270e8cf932faf
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-05-25 14:30:33 +0000

    feat: Eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import Head from 'next/head'
+
 import styles from '../styles/Home.module.css'
 
 const BACKEND_URL = process.env.BACKEND_URL || ''
 
-
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
